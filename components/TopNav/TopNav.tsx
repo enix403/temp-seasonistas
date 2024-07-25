55e1703efcaea283048e861036281fbe83a2930c
Hassan Imran <hassantube1@gmail.com>
2024-07-25 06:41:37 +0400

    linked top nav logo to home page

--- components/TopNav/TopNav.tsx
+++ components/TopNav/TopNav.tsx
@@ -36,11 +36,15 @@ function Contents({ pageTitle, showSearchButton }: TopNavProps) {
         <IconButton className="wl:hidden block" variant="text">
           <IconMenu2 className="w-20" />
         </IconButton>
-        <Image
-          alt=""
-          src={Logo}
-          className="h-7 w-auto lg:h-10 wl:block hidden"
-        />
+        <Link href="/">
+          <Image
+            alt=""
+            src={Logo}
+            
+            className="h-7 w-auto lg:h-10 wl:block hidden"
+          />
+        </Link>
+
         <div className="flex gap-x-3.5 items-center">
           {showSearchButton && (
             <IconButton className="wl:hidden block" variant="text">
