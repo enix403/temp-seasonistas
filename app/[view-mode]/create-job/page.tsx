1f55ee35d05f5440855341b831d362dcc4d7f25f
enix403 <fog.code000@gmail.com>
2024-08-22 16:43:13 +0500

    Make required changes

--- app/[view-mode]/create-job/page.tsx
+++ app/[view-mode]/create-job/page.tsx
@@ -36,7 +36,6 @@ export default function CreateJobPage({ params }: { params: any }) {
   return (
     <AppLayout pageTitle="Jobs" params={params}>
       <div className="flex-1 flex items-start">
-        {/* <Sidebar /> */}
         <div className="flex-1 px-7 py-8">{rendered}</div>
       </div>
     </AppLayout>
