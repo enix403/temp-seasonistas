87dfab228c2579d8c0da4588c197ccdea5bd5c1a
enix403 <fog.code000@gmail.com>
2024-09-07 13:32:47 +0500

    Add proper pricelists

--- app/[view-mode]/information/PriceListCells.tsx
+++ app/[view-mode]/information/PriceListCells.tsx
@@ -6,9 +6,11 @@ import {
   Avatar,
 } from "@material-tailwind/react";
 import clsx from "clsx";
+import { useContext } from "react";
+import { ViewModeContext } from "~/components/AppLayout/AppLayout";
 import { Button } from "~/components/Button/Button";
 
-export function Cell() {
+export function Cell({ title }: { title: string }) {
   return (
     <Card
       shadow={false}
@@ -22,7 +24,7 @@ export function Cell() {
           "absolute inset-0 m-0 h-full w-full rounded-none bg-cover bg-center",
         )}
         style={{
-          backgroundImage: 'url(https://images.unsplash.com/photo-1724599824488-ed1e81e11c1a?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)'
+          backgroundImage: 'url(https://images.unsplash.com/photo-1725308468426-9b5e227dc15c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw1OHx8fGVufDB8fHx8fA%3D%3D)'
         }}
       >
         <div className="to-bg-black-10 absolute inset-0 h-full w-full bg-gradient-to-t from-black/80 via-black/50" />
@@ -33,7 +35,7 @@ export function Cell() {
           color="white"
           className="mb-6 font-medium leading-[1.5]"
         >
-          CV management Price List
+          {title}
         </Typography>
         <Button className="mx-auto text-xl" fullRounded>
           View Price List
@@ -45,11 +47,14 @@ export function Cell() {
 
 
 export function PriceListCells() {
+  const viewMode = useContext(ViewModeContext);
   return (
     <section className="flex mt-8 gap-4">
-      <Cell />
-      <Cell />
-      <Cell />
+      <Cell title="CV Improvement Price List" />
+      <Cell title="CV Creation Price List" />
+      {viewMode === 'employer' && (
+        <Cell title="CV Management Price List" />
+      )}
     </section>
   );
 }
