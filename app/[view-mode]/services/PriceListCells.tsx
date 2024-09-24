ac8b66b112be1c408e25573dc00ef3eaf047d58b
enix403 <fog.code000@gmail.com>
2024-09-24 17:50:16 +0500

    Price list icons

--- app/[view-mode]/services/PriceListCells.tsx
+++ app/[view-mode]/services/PriceListCells.tsx
@@ -5,60 +5,48 @@ import {
   Typography,
   Avatar,
 } from "@material-tailwind/react";
+import {
+  IconFileInfo,
+  IconMessage2Share,
+  IconBoxMultiple5,
+  IconWritingSign,
+} from "@tabler/icons-react";
 import clsx from "clsx";
 import { useContext } from "react";
 import { ViewModeContext } from "~/components/AppLayout/AppLayout";
 import { Button } from "~/components/Button/Button";
 
-export function Cell({ title }: { title: string }) {
+export function Cell({ title, Icon }: { title: string; Icon: any }) {
   return (
-    <Card
-      shadow={false}
-      className="relative grid h-[34rem] w-full items-center justify-center overflow-hidden text-center"
-    >
-      <CardHeader
-        floated={false}
-        shadow={false}
-        color="transparent"
-        className={clsx(
-          "absolute inset-0 m-0 h-full w-full rounded-none bg-cover bg-center"
-        )}
-        style={{
-          backgroundImage:
-            "url(https://images.unsplash.com/photo-1725308468426-9b5e227dc15c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw1OHx8fGVufDB8fHx8fA%3D%3D)",
-        }}
+    <div className="flex-1 flex flex-col items-center">
+      <Icon size={60} />
+      <Typography
+        variant="h3"
+        color="black"
+        className="mb-6 mt-3 font-medium leading-[1.5]"
       >
-        <div className="to-bg-black-10 absolute inset-0 h-full w-full bg-gradient-to-t from-black/80 via-black/50" />
-      </CardHeader>
-      <CardBody className="relative py-14 px-6 md:px-12">
-        <Typography
-          variant="h2"
-          color="white"
-          className="mb-6 font-medium leading-[1.5]"
-        >
-          {title}
-        </Typography>
-        <Button className="mx-auto text-xl" fullRounded>
-          View Price List
-        </Button>
-      </CardBody>
-    </Card>
+        {title}
+      </Typography>
+      <Button className="mx-auto text-xl" fullRounded>
+        View Price List
+      </Button>
+    </div>
   );
 }
 
 export function PriceListCells() {
   const viewMode = useContext(ViewModeContext);
   return (
-    <section className="flex mt-8 gap-4">
+    <section className="flex mt-16 gap-4">
       {viewMode === "employer" ? (
         <>
-          <Cell title="CV Management" />
-          <Cell title="Job Postings Management" />
+          <Cell title="CV Management" Icon={IconFileInfo} />
+          <Cell title="Job Postings Management" Icon={IconMessage2Share} />
         </>
       ) : (
         <>
-          <Cell title="CV Improvement" />
-          <Cell title="CV Creation" />
+          <Cell title="CV Improvement" Icon={IconBoxMultiple5} />
+          <Cell title="CV Creation" Icon={IconWritingSign} />
         </>
       )}
     </section>
