5a13ed826b9abd8a648736ed29268248b86844e0
Fedi Labidi <92761733+fedilabidi13@users.noreply.github.com>
2024-12-16 02:40:46 +0100

    fixed certbot script

--- certbot-entrypoint.sh
+++ certbot-entrypoint.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
+set -e
 
-trap exit TERM
-while :; do
-  if [ ! -f /etc/letsencrypt/live/seasonistas.com/fullchain.pem ]; then
-    certbot certonly --webroot -w /var/www/certbot \
+# Check if certificates already exist
+if [ ! -f /etc/letsencrypt/live/seasonistas.com/fullchain.pem ]; then
+  echo "Certificates not found. Attempting to generate..."
+  certbot certonly --webroot -w /var/www/certbot \
     --email admin@seasonistas.com --agree-tos --no-eff-email \
     -d seasonistas.com -d www.seasonistas.com -d api.seasonistas.com
-  fi
-  sleep 12h & wait $!
-done
+else
+  echo "Certificates already exist. Skipping generation."
+fi
+
+echo "Certbot process complete. Exiting."
