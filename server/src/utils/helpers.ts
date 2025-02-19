91ab7f44e824ef87a36db656311ce30ee615b669
zaheer-bashir <zaheerkpr3@gmail.com>
2025-02-20 00:15:53 +0500

    add age validation to registration process

---
+++ server/src/utils/helpers.ts
@@ -0,0 +1,12 @@
+export const validateAge = (dateOfBirth) => {
+  const birthDate = new Date(dateOfBirth);
+  const today = new Date();
+  const age = today.getFullYear() - birthDate.getFullYear();
+  const monthDiff = today.getMonth() - birthDate.getMonth();
+  const dayDiff = today.getDate() - birthDate.getDate();
+
+  if (monthDiff < 0 || (monthDiff === 0 && dayDiff < 0)) {
+    return age - 1;
+  }
+  return age;
+};
