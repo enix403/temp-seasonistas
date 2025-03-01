6bcc5fa4fb8f7fab71504e0d5fff653ad1a3812e
enix403 <fog.code000@gmail.com>
2025-03-01 13:11:22 +0500

    Reorganize profile update page

--- client/app/[locale]/(main)/profile/page.tsx
+++ client/app/[locale]/(main)/profile/page.tsx
@@ -1,31 +1,20 @@
 "use client";
-import React, { ChangeEvent, useEffect, useRef, useState } from "react";
+
+import { useEffect, useRef, useState } from "react";
 
 import { MdOutlineAddAPhoto } from "react-icons/md";
 import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
+import { Input, Textarea } from "@material-tailwind/react";
 import toast from "react-hot-toast";
+import { useForm } from "react-hook-form";
+
 import { AppLayout } from "~/components/AppLayout/AppLayout";
 import { TitleMark } from "~/components/decorations";
-import { useAuthState } from "~/app/providers/auth-state";
 import { Button } from "~/components/Button/Button";
 import { apiRoutes } from "~/app/api-routes";
-import { useRouter } from "next/navigation";
-import { useForm } from "react-hook-form";
-import { Input, Textarea } from "@material-tailwind/react";
+import { useCurrentUser } from "~/app/hooks/useCurrentUser";
 
-const ProfilePageContent: React.FC = () => {
-  const { user, login, token } = useAuthState();
-
-  const [name, setName] = useState<string>(user?.fullName);
-  const [oldPassword, setOldPassword] = useState<string>("");
-  const [newPassword, setNewPassword] = useState<string>("");
-
-  const [isEditingPassword, setIsEditingPassword] = useState<boolean>(false);
-
-  const [loading, setLoading] = useState<boolean>(false);
-
-  const [showOldPassword, setShowOldPassword] = useState(false);
-  const [showNewPassword, setShowNewPassword] = useState(false);
+function ProfilePictureUpdater({ disabled = false }: { disabled?: boolean }) {
   const fileInputRef = useRef<HTMLInputElement | null>(null);
 
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -36,42 +25,139 @@ const ProfilePageContent: React.FC = () => {
         toast.error("Invalid file. Please upload an image.");
         return;
       }
-
-      handleProfileSave();
     } else {
       toast.error("No file selected.");
     }
   };
+
+  return (
+    <div
+      className={
+        "md:w-36 md:h-36 lg:mb-0 w-24 h-24 rounded-full bg-blue-500 shadow-xl overflow-hidden relative group border-white border-3"
+      }
+    >
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      <img
+        src={"/profile_empty_gradient.png"}
+        alt="Profile"
+        className="w-full h-full rounded-full object-cover"
+      />
+      <button
+        onClick={() => fileInputRef.current?.click()}
+        className={
+          "group-hover:opacity-100 opacity-0 transition-opacity bg-black/70 text-white absolute inset-0 flex items-center justify-center gap-x-2 font-bold text-sm"
+        }
+        disabled={disabled}
+      >
+        <MdOutlineAddAPhoto size={28} /> Change
+      </button>
+      <input
+        ref={fileInputRef}
+        type="file"
+        accept="image/*"
+        style={{ display: "none" }}
+        onChange={handleFileChange}
+        disabled={disabled}
+      />
+    </div>
+  );
+}
+
+function UpdateNameSection({
+  user,
+  userLoading,
+  onUpdate,
+}: {
+  user: any;
+  userLoading: boolean;
+  onUpdate: () => void;
+}) {
+  const [name, setName] = useState<string>();
+  const [loading, setLoading] = useState<boolean>(false);
+
   useEffect(() => {
-    setName(user?.fullName || "");
+    setName(user?.["fullName"] || "");
   }, [user]);
 
-  const router = useRouter();
   const handleProfileSave = async () => {
     if (!name || name.trim() === "") {
       toast.error("Please enter a valid user name");
       return;
     }
-    setLoading(true);
 
-    try {
-      await apiRoutes.updateProfile({ fullName: name });
-      login(token ?? "", { ...user, fullName: name });
-      toast.success("Profile successfully updated");
-      router.refresh();
-    } catch (error) {
-      if (error instanceof Error) {
-        toast.error(error.message);
-      } else {
-        toast.error("An unknown error occurred");
-      }
-    }
+    setLoading(true);
+    apiRoutes
+      .updateProfile({ fullName: name })
+      .then(() => {
+        toast.success("Name updated");
+        onUpdate();
+      })
+      .catch(() => {
+        toast.error("Failed to update name");
+      })
+      .finally(() => {
+        setLoading(false);
+      });
   };
+
+  return (
+    <div className="bg-white rounded-lg shadow-md p-6 mb-6">
+      <h2 className="text-xl font-semibold text-gray-900 mb-4">
+        Profile Information
+      </h2>
+      <div className="flex  flex-col space-y-4">
+        <div className="flex flex-row items-center gap-8">
+          <ProfilePictureUpdater disabled={loading} />
+          <div className="">
+            <h2 className="text-3xl font-semibold text-gray-900">
+              {user?.fullName}
+            </h2>
+            <p className="text-2xl text-gray-500">{user?.email}</p>
+          </div>
+        </div>
+        <div className="flex flex-col">
+          <div className="w-full">
+            <label className="block text-lg font-medium text-gray-700 mb-1">
+              Username
+            </label>
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              className="w-full border border-gray-300 text-gray-700 rounded-md p-2"
+            />
+          </div>
+
+          <div className="flex space-x-4">
+            <Button
+              onClick={() => handleProfileSave()}
+              loading={loading || userLoading}
+              className="px-4 py-2 mt-2 bg-primary text-teal rounded-md hover:bg-primary/80"
+            >
+              Save Changes
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function UpdatePasswordSection() {
+  const [isEditingPassword, setIsEditingPassword] = useState(false);
+
+  const [oldPassword, setOldPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+
+  const [showOldPassword, setShowOldPassword] = useState(false);
+  const [showNewPassword, setShowNewPassword] = useState(false);
 
   const handlePasswordSave = async () => {
     try {
       await apiRoutes.updatePassword({ oldPassword, newPassword });
       toast.success("Password successfully updated");
+      setOldPassword("");
+      setNewPassword("");
     } catch (error) {
       if (error instanceof Error) {
         toast.error(error.message);
@@ -82,179 +168,104 @@ const ProfilePageContent: React.FC = () => {
   };
 
   return (
-    <div className="p-6 bg-gray-50 min-h-screen">
-      <div className="app-container max-w-5xl w-full mb-12">
-        {/* Title */}
-        <h1 className="text-4xl text-center">
-          <span className="font-bold text-teal mr-1 relative bg-bdlue-600">
-            Profile
-            <TitleMark className="absolute w-32 top-full -left-5 dd -translate-y-2.5" />
-          </span>
-          {/* <span className="font-normal"> we are</span> */}
-        </h1>
-
-        {/* Subtitle */}
-        <p className="text-center text-xl font-normal text-black/50 mt-6 max-w-2xl mx-auto">
-          Edit your profile
-        </p>
-      </div>
-      {/* Profile Section */}
-      <div className="bg-white rounded-lg shadow-md p-6 mb-6">
-        <h2 className="text-xl font-semibold text-gray-900 mb-4">
-          Profile Information
-        </h2>
-        <div className="flex  flex-col space-y-4">
-          <div className="flex flex-row items-center gap-8">
-            <div
-              className={
-                "md:w-36 md:h-36 lg:mb-0 w-24 h-24 rounded-full bg-blue-500 shadow-xl overflow-hidden relative group border-white border-3"
-              }
+    <div className="bg-white rounded-lg shadow-md p-6 mb-6">
+      <h2 className="text-xl font-semibold text-gray-900 mb-4">
+        Update Password
+      </h2>
+      {isEditingPassword ? (
+        <div className="space-y-4 w-1/2">
+          <div className="relative">
+            <label className="block text-lg font-medium text-gray-700 mb-1">
+              Old Password
+            </label>
+            <input
+              type={showOldPassword ? "text" : "password"}
+              value={oldPassword}
+              onChange={(e) => setOldPassword(e.target.value)}
+              className="w-full border border-gray-300 text-gray-700 rounded-md p-2"
+            />
+            <span
+              onClick={() => setShowOldPassword((prev) => !prev)}
+              className="absolute right-3 top-[30px] z-[10] cursor-pointer"
             >
-              <img
-                src={"/profile_empty_gradient.png"}
-                alt="Profile"
-                className="w-full h-full rounded-full object-cover"
-              />
-              <button
-                onClick={() => fileInputRef.current?.click()}
-                className={
-                  "group-hover:opacity-100 opacity-0 transition-opacity bg-black/70 text-white absolute inset-0 flex items-center justify-center gap-x-2 font-bold text-sm"
-                }
-                disabled={loading}
-              >
-                <MdOutlineAddAPhoto size={28} /> Change
-              </button>
-              <input
-                ref={fileInputRef}
-                type="file"
-                accept="image/*"
-                style={{ display: "none" }}
-                onChange={handleFileChange}
-                disabled={loading}
-              />
-            </div>
-            <div className="">
-              <h2 className="text-3xl font-semibold text-gray-900">
-                {user?.fullName}
-              </h2>
-              <p className="text-2xl text-gray-500">{user?.email}</p>
-            </div>
+              {showOldPassword ? (
+                <AiOutlineEyeInvisible size={24} color="#000" />
+              ) : (
+                <AiOutlineEye size={24} color="#AFB2BF" />
+              )}
+            </span>
           </div>
-          <div className="flex flex-col">
-            <div className="w-full">
-              <label className="block text-lg font-medium text-gray-700 mb-1">
-                Username
-              </label>
-              <input
-                type="text"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                className="w-full border border-gray-300 text-gray-700 rounded-md p-2"
-              />
-            </div>
+          <div className=" relative">
+            <label className="block text-lg font-medium text-gray-700 mb-1">
+              New Password
+            </label>
+            <input
+              value={newPassword}
+              type={showNewPassword ? "text" : "password"}
+              onChange={(e) => setNewPassword(e.target.value)}
+              className="w-full border border-gray-300 text-gray-700 rounded-md p-2"
+            />
+            <span
+              onClick={() => setShowNewPassword((prev) => !prev)}
+              className="absolute right-3 top-[30px] z-[10] cursor-pointer"
+            >
+              {showNewPassword ? (
+                <AiOutlineEyeInvisible size={24} color="#AFB2BF" />
+              ) : (
+                <AiOutlineEye size={24} color="#AFB2BF" />
+              )}
+            </span>
+          </div>
 
-            <div className="flex space-x-4">
-              <Button
-                onClick={() => handleProfileSave()}
-                className="px-4 py-2 mt-2 bg-primary text-teal rounded-md hover:bg-primary/80"
-              >
-                Save Changes
-              </Button>
-            </div>
+          <div className="flex space-x-4">
+            <Button
+              onClick={handlePasswordSave}
+              className="px-4 py-2 mt-2 bg-primary text-white rounded-md hover:bg-primary/80"
+            >
+              Save Password
+            </Button>
+            <button
+              onClick={() => {
+                setIsEditingPassword(false);
+                setOldPassword("");
+                setNewPassword("");
+              }}
+              className="px-4 py-2 mt-2 bg-gray-200 text-gray-700 rounded-md hover:bg-gray-300"
+            >
+              Cancel
+            </button>
           </div>
         </div>
-      </div>
-
-      {/* Update Password Section */}
-      <div className="bg-white rounded-lg shadow-md p-6 mb-6">
-        <h2 className="text-xl font-semibold text-gray-900 mb-4">
-          Update Password
-        </h2>
-        {isEditingPassword ? (
-          <div className="space-y-4 w-1/2">
-            <div className="relative">
-              <label className="block text-lg font-medium text-gray-700 mb-1">
-                Old Password
-              </label>
-              <input
-                type={showOldPassword ? "text" : "password"}
-                value={oldPassword}
-                onChange={(e) => setOldPassword(e.target.value)}
-                className="w-full border border-gray-300 text-gray-700 rounded-md p-2"
-              />
-              <span
-                onClick={() => setShowOldPassword((prev) => !prev)}
-                className="absolute right-3 top-[30px] z-[10] cursor-pointer"
-              >
-                {showOldPassword ? (
-                  <AiOutlineEyeInvisible size={24} color="#000" />
-                ) : (
-                  <AiOutlineEye size={24} color="#AFB2BF" />
-                )}
-              </span>
-            </div>
-            <div className=" relative">
-              <label className="block text-lg font-medium text-gray-700 mb-1">
-                New Password
-              </label>
-              <input
-                value={newPassword}
-                type={showNewPassword ? "text" : "password"}
-                onChange={(e) => setNewPassword(e.target.value)}
-                className="w-full border border-gray-300 text-gray-700 rounded-md p-2"
-              />
-              <span
-                onClick={() => setShowNewPassword((prev) => !prev)}
-                className="absolute right-3 top-[30px] z-[10] cursor-pointer"
-              >
-                {showNewPassword ? (
-                  <AiOutlineEyeInvisible size={24} color="#AFB2BF" />
-                ) : (
-                  <AiOutlineEye size={24} color="#AFB2BF" />
-                )}
-              </span>
-            </div>
-
-            <div className="flex space-x-4">
-              <Button
-                onClick={handlePasswordSave}
-                className="px-4 py-2 mt-2 bg-primary text-white rounded-md hover:bg-primary/80"
-              >
-                Save Password
-              </Button>
-              <button
-                onClick={() => {
-                  setIsEditingPassword(false);
-                  setOldPassword("");
-                  setNewPassword("");
-                }}
-                className="px-4 py-2 mt-2 bg-gray-200 text-gray-700 rounded-md hover:bg-gray-300"
-              >
-                Cancel
-              </button>
-            </div>
-          </div>
-        ) : (
-          <button
-            onClick={() => setIsEditingPassword(true)}
-            className="px-4 py-2 mt-2 bg-gray-200 text-gray-700 rounded-md hover:bg-gray-300"
-          >
-            Change Password
-          </button>
-        )}
-      </div>
-
-      <UpdateBioSection user={user} />
+      ) : (
+        <button
+          onClick={() => setIsEditingPassword(true)}
+          className="px-4 py-2 mt-2 bg-gray-200 text-gray-700 rounded-md hover:bg-gray-300"
+        >
+          Change Password
+        </button>
+      )}
     </div>
   );
-};
+}
 
-function UpdateBioSection({ user }: { user: any }) {
-  const { register, handleSubmit } = useForm({
+function UpdateBioSection({
+  user,
+  userLoading,
+  onUpdate,
+}: {
+  user: any;
+  userLoading: boolean;
+  onUpdate: () => void;
+}) {
+  const { register, handleSubmit, setValue } = useForm({
     defaultValues: {
       bio: user?.["bio"] ?? "",
     },
   });
+
+  useEffect(() => {
+    setValue("bio", user?.["bio"] || "");
+  }, [user]);
 
   const [loading, setLoading] = useState(false);
 
@@ -265,6 +276,7 @@ function UpdateBioSection({ user }: { user: any }) {
       .updateProfile({ bio })
       .then(() => {
         toast.success("Bio updated");
+        onUpdate();
       })
       .catch(() => {
         toast.error("Failed to update bio");
@@ -279,41 +291,52 @@ function UpdateBioSection({ user }: { user: any }) {
       <h2 className="text-xl font-semibold text-gray-900 mb-4">Update Bio</h2>
       <form onSubmit={handleSubmit(onSubmit)} className="space-y-4 w-1/2">
         <Textarea {...register("bio")} label="Bio" />
-        <Button loading={loading}>Update Bio</Button>
+        <Button loading={loading || userLoading}>Update Bio</Button>
       </form>
     </div>
   );
 }
 
-/*
-const router = useRouter();
-const handleProfileSave = async () => {
-  if (!name || name.trim() === "") {
-    toast.error("Please enter a valid user name");
-    return;
-  }
-  setLoading(true);
+function UpdateSections() {
+  const { user, isLoading, refreshUser } = useCurrentUser();
 
-  try {
-    await apiRoutes.updateProfile({ fullName: name });
-    login(token ?? "", { ...user, fullName: name });
-    toast.success("Profile successfully updated");
-    router.refresh();
-  } catch (error) {
-    if (error instanceof Error) {
-      toast.error(error.message);
-    } else {
-      toast.error("An unknown error occurred");
-    }
-  }
-};
-
-*/
+  return (
+    <>
+      <UpdateNameSection
+        user={user}
+        userLoading={isLoading}
+        onUpdate={refreshUser}
+      />
+      <UpdatePasswordSection />
+      <UpdateBioSection
+        user={user}
+        userLoading={isLoading}
+        onUpdate={refreshUser}
+      />
+    </>
+  );
+}
 
 export default function ProfilePage() {
   return (
     <AppLayout>
-      <ProfilePageContent />
+      <div className="p-6 bg-gray-50 min-h-screen">
+        {/* ========== Static Header ========== */}
+        <div className="app-container max-w-5xl w-full mb-12">
+          <h1 className="text-4xl text-center">
+            <span className="font-bold text-teal mr-1 relative bg-bdlue-600">
+              Profile
+              <TitleMark className="absolute w-32 top-full -left-5 dd -translate-y-2.5" />
+            </span>
+          </h1>
+
+          <p className="text-center text-xl font-normal text-black/50 mt-6 max-w-2xl mx-auto">
+            Edit your profile
+          </p>
+        </div>
+
+        <UpdateSections />
+      </div>
     </AppLayout>
   );
 }
