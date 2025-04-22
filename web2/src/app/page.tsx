3c5e265ee0146c84ffd3ccf7a9124bf668709ee0
enix403 <fog.code000@gmail.com>
2025-04-23 00:47:27 +0500

    Create landing page

--- web2/src/app/page.tsx
+++ web2/src/app/page.tsx
@@ -17,10 +17,20 @@ function Hero() {
   return (
     <div
       className={clsx(
-        "min-h-screen bg-[#022127] py-20 text-[#DBDBDB]",
-        "px-6 md:px-10 lg:px-20"
+        "min-h-screen py-20",
+        "px-6 md:px-10 lg:px-20",
+        "text-[#DBDBDB]",
+        "bg-[#022127]",
+        "relative"
+        // <div class="relative h-full w-full bg-neutral-900"></div>
       )}
     >
+      <div
+        className={clsx(
+          "absolute top-1/2 left-1/2 h-[95%] w-[60%] bg-[#dedddd] bg-[size:10%_10%] opacity-5 blur-[1000px]",
+          "-translate-x-1/2 -translate-y-1/2"
+        )}
+      ></div>
       <div
         className={clsx(
           "w-fit rounded-full border border-[#3D5C64] bg-white/[0.06] px-5 py-2",
@@ -35,7 +45,7 @@ function Hero() {
 
       <div className='flex flex-col items-start gap-x-10 max-xl:justify-between md:flex-row xl:gap-x-60 2xl:gap-x-80'>
         {/* Main Text */}
-        <h1 className='mt-4 text-4xl font-semibold leading-[105%] text-white sm:text-5xl md:text-6xl xl:text-7xl'>
+        <h1 className='mt-4 text-4xl leading-[105%] font-semibold text-white sm:text-5xl md:text-6xl xl:text-7xl'>
           <HeroShadow>Finding</HeroShadow>{" "}
           <AuroraText colors={["#81A7EB", "#956ED4", "#FE605D", "#F3C44D"]}>
             Seasonal
