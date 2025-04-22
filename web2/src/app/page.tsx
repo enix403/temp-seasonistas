d7a0e19582ee150cafdd550687a20acd3f47c5a7
enix403 <fog.code000@gmail.com>
2025-04-23 01:53:16 +0500

    Create landing page

--- web2/src/app/page.tsx
+++ web2/src/app/page.tsx
@@ -18,8 +18,6 @@ function HeroShadow({ children }: PropsWithChildren) {
 }
 
 function Hero() {
-  const [ref, { width, height }] = useMeasure();
-
   return (
     <>
       <div
@@ -73,53 +71,46 @@ function Hero() {
             <p className='mt-6 max-w-md text-lg md:text-xl'>
               Seasonistas is a platform that connects seasonal workers and
               employers across Greece, helping them find the ideal job or the
-              right person for their team. {width} {height}
+              right person for their team.
             </p>
             <Button size='lg'>Get Started Now</Button>
           </div>
         </div>
 
-        {/* Fake for measure only */}
-        <div className='invisible relative'>
-          <div
-            className={clsx(
-              "mx-auto mt-14 w-full max-w-7xl rounded-t-xl border-[0.5px] border-[#80D6E2]/30 bg-white/10 px-6",
-              "absolute"
-            )}
-          >
-            <img src='/hero-mobile.png' className='invisible w-full' />
-            <div className='absolute top-0 left-0 h-1/2 w-full'>
-              <img
-                ref={ref}
-                src='/hero-mobile.png'
-                className='h-full w-full object-cover object-top'
-              />
-            </div>
-          </div>
-        </div>
+        <div className='h-10' />
 
-        <div className='' style={{ height: height ?? 0 }}></div>
-      </div>
-      <div
-        className='relative flex px-6 md:px-10 lg:px-20'
-        style={{ translate: `0 -${height}px` }}
-      >
-        <div
-          className={clsx(
-            "absolute w-full max-w-7xl rounded-t-xl border-[0.5px] border-[#80D6E2]/30 bg-white/10"
-          )}
-          style={{ height: height ?? 0 }}
-        ></div>
-
-        <div className='mx-auto max-w-7xl d flex-1 px-6 pt-6'>
-          <img
-            src='/hero-mobile.png'
-            className='w-full object-cover object-top'
-          />
-        </div>
-        {/*  */}
+        {/* <img src='/hero-mobile.png' className='w-full' /> */}
       </div>
     </>
+  );
+}
+
+function AboutUs() {
+  return (
+    <div className={clsx("py-10", "lg:sx-24 px-8 md:px-14")}>
+      <div className='mx-auto flex max-w-7xl flex-col items-start gap-y-12 md:flex-row md:gap-x-14 lg:gap-x-28'>
+        <div className='flex-1 max-md:w-full max-md:px-8 max-md:order-1'>
+          <img src='/about-us.png' className='w-full max-w-full' />
+        </div>
+
+        <div className='flex-1 shrink-0 space-y-4'>
+          <h1 className='max-md:text-4xl'>Connecting you is our job</h1>
+          <h3>Connecting Seasonal Talent with Opportunity</h3>
+          <div className='text-lg font-normal text-[#49565A]'>
+            <p>
+              Seasonistas was created by a team of people with firsthand
+              experience in seasonal work and the challenges that come with it.
+            </p>
+            <p>
+              That’s exactly why we built a platform that helps seasonal
+              professionals find the right job and businesses build the right
+              team.
+            </p>
+          </div>
+          <Button size='lg'>Get Started Now</Button>
+        </div>
+      </div>
+    </div>
   );
 }
 
@@ -127,7 +118,7 @@ export default function Home() {
   return (
     <div className={clsx("max-h-full overflow-y-auto")}>
       <Hero />
-      <p>Hello</p>
+      <AboutUs />
       {/*  <div className='relative -top-[25vw] px-6 md:px-10 lg:px-20'>
         <div className='bg-red-400 p-6'>
           <img src='/hero-mobile.png' className='w-full' />
