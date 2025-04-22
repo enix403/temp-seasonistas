f890606298861a70e03c7c2723b64212dde12aa9
enix403 <fog.code000@gmail.com>
2025-04-23 00:07:32 +0500

    hero banner

--- web2/src/app/layout.tsx
+++ web2/src/app/layout.tsx
@@ -1,16 +1,106 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+// import { Geist, Geist_Mono } from "next/font/google";
+import localFont from "next/font/local";
+
 // import "@/app/globals.css";
 import "@/styles/global.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"]
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"]
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"]
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"]
+// });
+
+/* prettier-ignore */
+export const metropolis = localFont({
+  src: [
+    {
+      path: "../../public/fonts/metropolis/Metropolis-Thin.otf",
+      weight: "100",
+      style: "normal"
+    },
+    {
+      path: "../../public/fonts/metropolis/Metropolis-ThinItalic.otf",
+      weight: "100",
+      style: "italic"
+    },
+    {
+      path: "../../public/fonts/metropolis/Metropolis-Light.otf",
+      weight: "300",
+      style: "normal"
+    },
+    {
+      path: "../../public/fonts/metropolis/Metropolis-LightItalic.otf",
+      weight: "300",
+      style: "italic"
+    },
+    {
+      path: "../../public/fonts/metropolis/Metropolis-Regular.otf",
+      weight: "400",
+      style: "normal"
+    },
+    {
+      path: "../../public/fonts/metropolis/Metropolis-RegularItalic.otf",
+      weight: "400",
+      style: "italic"
+    },
+    {
+      path: "../../public/fonts/metropolis/Metropolis-Medium.otf",
+      weight: "500",
+      style: "normal"
+    },
+    {
+      path: "../../public/fonts/metropolis/Metropolis-MediumItalic.otf",
+      weight: "500",
+      style: "italic"
+    },
+    {
+      path: "../../public/fonts/metropolis/Metropolis-SemiBold.otf",
+      weight: "600",
+      style: "normal"
+    },
+    {
+      path: "../../public/fonts/metropolis/Metropolis-SemiBoldItalic.otf",
+      weight: "600",
+      style: "italic"
+    },
+    {
+      path: "../../public/fonts/metropolis/Metropolis-Bold.otf",
+      weight: "700",
+      style: "normal"
+    },
+    {
+      path: "../../public/fonts/metropolis/Metropolis-BoldItalic.otf",
+      weight: "700",
+      style: "italic"
+    },
+    {
+      path: "../../public/fonts/metropolis/Metropolis-ExtraBold.otf",
+      weight: "800",
+      style: "normal"
+    },
+    {
+      path: "../../public/fonts/metropolis/Metropolis-ExtraBoldItalic.otf",
+      weight: "800",
+      style: "italic"
+    },
+    {
+      path: "../../public/fonts/metropolis/Metropolis-Black.otf",
+      weight: "900",
+      style: "normal"
+    },
+    {
+      path: "../../public/fonts/metropolis/Metropolis-BlackItalic.otf",
+      weight: "900",
+      style: "italic"
+    }
+  ],
+  variable: "--font-metropolis",
+  display: "swap"
 });
 
 export const metadata: Metadata = {
@@ -25,11 +115,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`${metropolis.variable} antialiased`}>{children}</body>
     </html>
   );
 }
--- web2/src/app/page.tsx
+++ web2/src/app/page.tsx
@@ -1,31 +1,52 @@
-import { AnimatedGradientText } from "@/components/magicui/animated-gradient-text";
 import { AuroraText } from "@/components/magicui/aurora-text";
 import { SparklesText } from "@/components/magicui/sparkles-text";
 import { Button } from "@/components/ui/button";
-import { repeatNode } from "@/lib/utils";
 import clsx from "clsx";
-import Image from "next/image";
+import { CloudLightning } from "lucide-react";
+import { PropsWithChildren } from "react";
 
-/*
-bg-[#022127]
-*/
-
-export default function Home() {
+function HeroShadow({ children }: PropsWithChildren) {
   return (
-    <div className={clsx("max-h-full space-y-6 overflow-y-auto p-20")}>
-      <h1 className='text-6xl leading-16 font-semibold'>
-        Finding{" "}
-        <AuroraText
-          // className='font-semibold'
-          colors={["#81A7EB", "#956ED4", "#FE605D", "#F3C44D"]}
-        >
+    <span className='text-shadow-[0px_4px_4px_rgba(0,0,0,0.25)]'>
+      {children}
+    </span>
+  );
+}
+
+function Hero() {
+  return (
+    <div className='min-h-screen bg-[#022127] px-(--spacing-hero) py-20 text-white'>
+      <div
+        className={clsx(
+          "w-fit rounded-full border border-[#3D5C64] bg-white/[0.06] px-5 py-2 text-[#DBDBDB]",
+          "flex items-center gap-x-3 text-sm"
+        )}
+      >
+        <span className='text-[#2062A6]'>
+          <CloudLightning fill='currentColor' size={20} stroke='currentColor' />
+        </span>
+        Hire Smart Faster
+      </div>
+      <h1 className='mt-4 text-6xl leading-16 font-semibold'>
+        <HeroShadow>Finding</HeroShadow>{" "}
+        <AuroraText colors={["#81A7EB", "#956ED4", "#FE605D", "#F3C44D"]}>
           Seasonal
           <br /> Work
         </AuroraText>{" "}
-        Made
+        <HeroShadow>Made</HeroShadow>
         <br />
-        <SparklesText>Simple</SparklesText>
+        <SparklesText>
+          <HeroShadow>Simple</HeroShadow>
+        </SparklesText>
       </h1>
+    </div>
+  );
+}
+
+export default function Home() {
+  return (
+    <div className={clsx("max-h-full overflow-y-auto")}>
+      <Hero />
     </div>
   );
 }
