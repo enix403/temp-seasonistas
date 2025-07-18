882866e09de70ec4093a9d287b4939db1cebfd6b
Qateef Ahmad <qateef.ahmad@educative.io>
2025-07-18 23:37:08 +0500

    change

---
+++ client/src/stores/profileAtoms.ts
@@ -0,0 +1,35 @@
+import { atom } from 'jotai';
+
+export interface IndividualProfileData {
+  email: string;
+  gender: string;
+  phoneNumber: string;
+  location: string;
+  website: string;
+}
+
+export interface CompanyProfileData {
+  website: string;
+  industry: string;
+  companyType: string;
+  companySize: string;
+  headquarters: string;
+  yearOfEstablishment: string;
+}
+
+export const individualProfileAtom = atom<IndividualProfileData>({
+  email: '',
+  gender: '',
+  phoneNumber: '',
+  location: '',
+  website: '',
+});
+
+export const companyProfileAtom = atom<CompanyProfileData>({
+  website: '',
+  industry: '',
+  companyType: '',
+  companySize: '',
+  headquarters: '',
+  yearOfEstablishment: '',
+});
